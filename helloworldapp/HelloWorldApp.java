794e7e71343af4e048b5c10d3d5530f009741920
Aviel Resnick <aviel.resnick@gmail.com>
2017-02-03 08:15:14 -0500

    Hello Word!

---
+++ helloworldapp/HelloWorldApp.java
@@ -0,0 +1,18 @@
+package helloworldapp;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ * 
+ * @author Aviel Resnick
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
